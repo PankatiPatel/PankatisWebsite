d902c3010c7e716f4dd71ed95a3e38c069628c71
pankati patel <pankatipatel08@gmail.com>
2021-03-25 18:35:15 -0400

    My personal website
    My personal website done in html php javascript css

---
+++ submitForm.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_POST["yourName"]))
+echo "My name is : " . $_POST['yourName']. "</br>";
+
+
+if(isset($_POST["email"]))
+echo "My email is : " . $_POST['email']. "</br>";
+
+if(isset($_POST["message"]))
+echo "My email is : " . $_POST['message']. "</br>";
+
+?>
